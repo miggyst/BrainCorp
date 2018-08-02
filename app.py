b31f8b00b085ab2b657a83dbdf11aef7b259f0fc
Santos-Tankia <AS063282@cerner.net>
2018-08-02 15:20:05 -0500

    Checked out windows branch, so that I can make a working copy for windows while I'm away from my unix machine. Added new users/query?<param> functionality with testing; Updated documentation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+import os
+
 app = Flask(__name__)
 
 '''
@@ -20,6 +22,19 @@ def index():
 @app.route('/users', methods=['GET'])
 def getUsers():
     userList = userListFromPwdFile()
+    if userList == 'Error':
+        return 'ERROR'
+    else:
+        return jsonify(userList)
+
+
+'''
+<summary>GET request for /users/query link</summary>
+<returns>List of user information in readable JSON format</returns>
+'''
+@app.route('/users/query', methods=['GET'])
+def getUsersQuery():
+    userList = usersQuery()
     return jsonify(userList)
 
 '''
@@ -27,20 +42,60 @@ def getUsers():
 <returns>List object of user information except for the encrypted password</returns>
 '''
 def userListFromPwdFile():
-    with open('/etc/passwd') as file:
-        userList = file.read().splitlines()
-    for i in range(len(userList)):
-        userList[i] = userList[i].split(':')
-        userList[i] = {
-            'name':userList[i][0],
-            'uid':userList[i][2],
-            'gid':userList[i][3],
-            'comment':userList[i][4],
-            'home':userList[i][5],
-            'shell':userList[i][6]
-        }
+    if os.name == 'nt':
+        with open('example.txt') as file:
+            userList = file.read().splitlines()
+        for i in range(len(userList)):
+            userList[i] = userList[i].split(':')
+            userList[i] = {
+                'name':userList[i][0],
+                'uid':userList[i][2],
+                'gid':userList[i][3],
+                'comment':userList[i][4],
+                'home':userList[i][5],
+                'shell':userList[i][6]
+            }
+    else:
+        with open('/etc/passwd') as file:
+            userList = file.read().splitlines()
+        for i in range(len(userList)):
+            userList[i] = userList[i].split(':')
+            userList[i] = {
+                'name':userList[i][0],
+                'uid':userList[i][2],
+                'gid':userList[i][3],
+                'comment':userList[i][4],
+                'home':userList[i][5],
+                'shell':userList[i][6]
+            }
     return userList
 
+'''
+<summary>
+The usersQuery function searches through current userList found in /etc/passwd and cross references GET request parameters to find specific users
+Must also be tied to a GET request function call since it requires GET request arguments
+</summary>
+<returns>List of user objects</returns>
+'''
+def usersQuery():
+    PARAMETERS = ['name', 'uid', 'gid', 'comment', 'home', 'shell']
+    paramList = []
+    usersQueryList = []
+    userList = userListFromPwdFile()
+    try:
+        for params in PARAMETERS:
+            if request.args.get(params) is not None:
+                paramList.append(params)
+        for jsonObject in userList:
+            count = 0
+            for params in paramList:
+                if jsonObject[params] == request.args.get(params):
+                    count += 1
+            if count == len(paramList) and count != 0:
+                usersQueryList.append(jsonObject)
+        return usersQueryList
+    except Exception:
+        return 'Error'
 
 '''
 <summary>Main Function that runs the flask service application</summary>
--- appTest.py
+++ appTest.py
@@ -4,40 +4,56 @@ import json
 import urllib.request
 from flask import jsonify
 
+import os
+import requests
+
 class appUnitTest(unittest.TestCase):
 
     '''
     <summary>Basic unit test for index function within app.py</summary>
-    <asserts>assertEqual; String comparison<asserts>
-    <returns>Success if return string is: Hello World!</returns>
+    <assert>Success if return string is: Hello World!</assert>
     '''
     def testIndex(self):
         self.assertEqual(app.index(),'Hello World!')
 
+
     '''
     <summary>Unit test for getUsers() function within app.py; Will only work if server is running</summary>
-    <asserts>assertTrue; Checks for json object</asserts>
-    <returns>Success if request returns a JSON object</returns>
+    <assert>Success if request returns a JSON object</assert>
     '''
     def testGetUsers(self):
         contents = urllib.request.urlopen('http://127.0.0.1:5000/users').read()
         self.assertTrue(self.isJsonObject(contents))
     
+
+    '''
+    <summary>Unit test for getUsersQuery() function within app.py; Will only work if server is running</summary>
+    <assert>Success if request returns a JSON object that matches the root JSON object specified</assert>
+    '''
+    def testGetUsersQuery(self):
+        contents = urllib.request.urlopen('http://127.0.0.1:5000/users/query?name=root').read()
+        contents = (contents.decode('utf-8')).strip(' \\t\\n\\r')
+        contents = json.loads(contents)
+        jsonObject = json.loads('[{"comment": "root","gid": "0","home": "/root","name": "root","shell": "/bin/bash","uid": "0"}]')
+        self.assertTrue(jsonObject, contents)
+
+
     '''
     <summary>Unit test for userListFromPwdFile() function within app.py</summary>
-    <asserts>assertEqual; List comparison</asserts>
-    <asserts>assertTrue; Checks if list is empty</asserts>
-    <returns>
+    <assert>
     assertEqual = Success if return list is the same as the list found in /etc/passwd
     assertTrue = Success if return list is NOT empty
-    </returns>
+    </assert>
     '''
     def testUserListFromPwdFile(self):
         userListFromPwdFile = app.userListFromPwdFile()
         self.assertTrue(len(userListFromPwdFile) != 0)
-        
-        with open('/etc/passwd') as file:
-            userList = file.read().splitlines()
+        if os.name == 'nt':
+            with open('example.txt') as file:
+                userList = file.read().splitlines()
+        else:    
+            with open('/etc/passwd') as file:
+                userList = file.read().splitlines()
         for i in range(len(userList)):
             userList[i] = userList[i].split(':')
             userList[i] = {
@@ -49,7 +65,17 @@ class appUnitTest(unittest.TestCase):
                 'shell':userList[i][6]
             }
         self.assertEqual(userList, userListFromPwdFile)
-    
+
+
+    '''
+    <summary>Unit test for usersQuery() function within app.py</summary>
+    <assert>Success if it returns an error; Since the function requires an HTTP call beforehand, the error handler would be called</assert>
+    '''
+    def testUsersQuery(self):
+        test = app.usersQuery()
+        self.assertEqual('Error', test)
+
+
     '''
     <summary>Checks whether or not an object is a JSON object</summary>
     <param name = 'jsonObject'>jsonObject is top be checked whether or not it is of a JSON format</param>
